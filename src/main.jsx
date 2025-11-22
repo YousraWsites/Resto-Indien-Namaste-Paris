8c373432b6b88133352fef4206409ff8ad2b234d
Yousra <yousrahachmi2004@gmail.com>
2025-11-22 19:47:49 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CartProvider } from './contexts/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <CartProvider>
         <App />
       </CartProvider>
